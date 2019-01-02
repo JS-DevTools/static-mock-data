277c14a546a53f26ab78e2dc3aedcaf1c8b3e5db
Randall Krauskopf <randall.krauskopf@gmail.com>
2019-01-01 18:12:48 -0600

    Initial commit to begin converting the project to Typescript and Rollup.

---
+++ rollup.config.dev.js
@@ -0,0 +1,25 @@
+import typescript from "rollup-plugin-typescript2";
+import json from "rollup-plugin-json";
+import { terser } from "rollup-plugin-terser";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+
+export default {
+
+  input: "build/browser.js",
+
+  output: {
+    file: "out/static-mock-data.js",
+    format: "umd",
+    name: "mock.data",
+    sourcemap: true
+
+  },
+
+  plugins: [
+    resolve(),
+    commonjs(),
+    json()
+    // typescript()
+  ]
+}
---
+++ rollup.config.prod.js
@@ -0,0 +1,30 @@
+import typescript from "rollup-plugin-typescript2";
+import json from "rollup-plugin-json";
+import { terser } from "rollup-plugin-terser";
+import nodeResolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+
+
+export default {
+  input: "build/browser.js",
+
+  output: {
+    file: "out/static-mock-data.min.js",
+    format: "umd",
+    name: "mock.data",
+    sourcemap: false
+  },
+
+  plugins: [
+    commonjs(),
+    nodeResolve(),
+    json(),
+    typescript(),
+    terser({
+      ecma: 8,
+      output: {
+        comments: /^!/,
+      },
+    })
+  ]
+};
