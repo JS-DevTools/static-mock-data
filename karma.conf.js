30f4ea96057807a8e2fadb101a47821a56646bc5
James Messinger <james.messinger@gmail.com>
2019-02-03 08:04:08 -0600

    Testing karma-config

--- karma.conf.js
+++ karma.conf.js
@@ -1,52 +1,15 @@
 // Karma config
 // https://karma-runner.github.io/0.12/config/configuration-file.html
+// https://jsdevtools.org/karma-config/
 
-// @ts-check
 "use strict";
+const karmaConfig = require("karma-config").default;
 
-// const karmaConfig = require("karma-config").default;
-//
-// module.exports = karmaConfig({
-//   sourceDir: "esm",
-// });
-
-module.exports = (karma) => karma.set({
-  frameworks: ["mocha", "chai", "host-environment"],
-  reporters: ["verbose", "coverage-istanbul"],
-  files: [
-    "test/**/*.+(spec|test).+(js|jsx)",
-    { pattern: "test/**/*", included: false, served: true }
-  ],
-  preprocessors: {
-    "test/**/*.+(spec|test).+(js|jsx)": ["webpack"],
-  },
-  webpack: {
-    mode: "development",
-    devtool: "inline-source-map",
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          include: /esm/,
-          exclude: /node_modules|\.spec\.|\.test\./,
-          enforce: "post",
-          use: "coverage-istanbul-loader",
-        }
-      ]
-    },
-  },
-  browsers: [
-    "FirefoxHeadless",
-    "ChromeHeadless",
-    // "Edge"
-  ],
-
-  coverageIstanbulReporter: {
-    skipFilesWithNoCoverage: true,
-    dir: "coverage/%browser%",
-    reports: [
-      "text-summary",
-      "lcov"
-    ]
-  },
+module.exports = karmaConfig({
+  sourceDir: "esm",
+  // config: {
+  //   coverageIstanbulReporter: {
+  //     skipFilesWithNoCoverage: true,
+  //   }
+  // }
 });
