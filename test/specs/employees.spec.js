c0e9be5b724bc0f131c25947fe8f995e5b1f9a5d
James Messinger <james.messinger@gmail.com>
2020-03-06 06:25:43 -0600

    Switched to scoped @JSDevTools packages

--- test/specs/employees.spec.js
+++ test/specs/employees.spec.js
@@ -3,7 +3,7 @@
 const staticMockData = require("../../");
 const employeeJSON = require("../../employees.json");
 const { expect } = require("chai");
-const host = require("host-environment");
+const host = require("@jsdevtools/host-environment");
 
 describe("employees", () => {
   it("should be two separate data sources", () => {
